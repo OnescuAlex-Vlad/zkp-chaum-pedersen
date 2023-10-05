dbc8f1e1e863793aa590ef8f3c36d3acc6b6d729
OnescuAlex-Vlad <onescu8779@gmail.com>
2023-10-05 16:15:59 +0300

    Create server & client bins

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .out_dir("src/")
-        .compile(&["proto/zkp_auth.proto"], &["proto/"])
+        .compile(&["proto/zkp.proto"], &["proto/"])
         .unwrap();
 }
